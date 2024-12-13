61bf0a843ec8280e2e04e3cdc7482b022a5c4ab1
Luca Bulletti <luca.bulletti@garaio-rem.ch>
2024-12-13 15:01:20 +0100

    day 8 part 2

--- 2024/8/solution.rb
+++ 2024/8/solution.rb
@@ -33,13 +33,41 @@ def antinodes(nodes)
   [a1, a2]
 end
 
+def harmonic_antinodes(nodes)
+  node1, node2 = nodes
+  distance_x = (node2.x - node1.x)
+  distance_y = (node2.y - node1.y)
+  antinodes = [node1, node2]
+  i = j = 1
+  a1 = Node.new(node1.x - (distance_x * i), node1.y - (distance_y * i))
+  a2 = Node.new(node2.x + (distance_x * j), node2.y + (distance_y * j))
+  while(!a1.out_of_bounds?)
+    antinodes << a1
+    i += 1
+    a1 = Node.new(node1.x - (distance_x * i), node1.y - (distance_y * i))
+  end
+
+  while(!a2.out_of_bounds?)
+    antinodes << a2
+    j += 1
+    a2 = Node.new(node2.x + (distance_x * j), node2.y + (distance_y * j))
+  end
+
+  antinodes
+end
+
 all_antinodes = []
+all_harmonic_antinodes = []
 
 antennae.each do |_frequency, antennas|
   antennas = antennas.map { Node.new(_1.first, _1.last) }
   antinodes = antennas.combination(2).flat_map { antinodes(_1) }
   all_antinodes << antinodes.reject(&:out_of_bounds?)
+  # part 2
+  all_harmonic_antinodes << antennas.combination(2).flat_map { harmonic_antinodes(_1) }
 end
 
 all_antinodes.flatten!
+all_harmonic_antinodes.flatten!
 puts all_antinodes.uniq{ [_1.x, _1.y] }.count
+puts all_harmonic_antinodes.uniq{ [_1.x, _1.y] }.count
